73a7d3b0367e31fbf8cab73a1a0f55489403ecc3
devillers <davidevillers@gmail>
2025-06-25 02:24:20 +0200

    trt

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,21 @@
-//app/layout.js
-
 import './globals.css'
 import { LayoutProvider } from './LayoutContext'
 import LayoutWrapper from './LayoutWrapper'
 import Providers from './providers'
+import Head from 'next/head'
 
 export const metadata = {
   title: 'Pause Réflexo',
   description: '...',
-  icons: [
-    { rel: 'apple-touch-icon', url: '/apple-touch-icon.png' },
-    { rel: 'apple-touch-icon-precomposed', url: '/apple-touch-icon-precomposed.png' },
-  ],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <Head>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-precomposed.png" />
+      </Head>
       <body className="relative" suppressHydrationWarning={true}>
         <Providers>
           <LayoutProvider>
